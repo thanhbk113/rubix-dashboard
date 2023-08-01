5d3d245f00cf0a0af40276a046f5714e5a47500f
thanhbk113 <thanhbk113@gmail.com>
2023-08-01 20:17:43 +0700

    change api link

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,6 @@
 export const ERROR_TOKEN = 'RefreshAccessTokenError';
 
-export const BASE_URL_API = 'https://hieuhoine.onrender.com';
+export const BASE_URL_API = 'https://tmdt-api-7i6b.onrender.com';
 
 export const ROUTES = {
   LOGIN: '/login',
